b3c7cb55c8802f1197e8b58770283445612919c6
ayomide <sulaimanayomide005@gmail.com>
2025-04-26 20:09:10 +0100

    useState

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,10 +1,18 @@
+import { useState } from "react";
+
 const Home = () => {
+
+    const [name, setNaame] = useState("Hamid")
+    const [age, setAge] = useState(20)
+
     const handleClick = (f) =>{
         console.log("Hello, Ninja", f.target);
+        setNaame("Ayomide")
     }
 
     const handleClickAgain = (name, e) => {
         console.log(`Hello ${name}`, e.target);
+        setAge(19)
     }
 
     return (
@@ -12,6 +20,7 @@ const Home = () => {
             <h1>Home Page</h1>
             <button onClick={handleClick}> Click Me </button>
             <button onClick={(e) => handleClickAgain("Maria", e)}>Click Me Again</button>
+            <p>{name} is {age} years old</p>
         </div>
     )
 }
