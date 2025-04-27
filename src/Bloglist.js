f6ff9d86f1d14e48e0402c3904b605b813ba5fce
ayomide <sulaimanayomide005@gmail.com>
2025-04-27 14:24:41 +0100

    Props Done

---
+++ src/Bloglist.js
@@ -0,0 +1,20 @@
+const Bloglist = (props) => {
+    const { blogs } = props; // Destructure the blogs prop
+    // const blogs = props.blogs; // Alternatively, you can use this line to access the blogs prop
+    // const blogs = [ // Sample data for testing
+    const title = props.title; // Destructure the title prop
+    return (
+        <div className="Bloglist">
+            <h1>{title}</h1>
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h1>{blog.title}</h1>
+                    <p>Written by {blog.author}</p>
+                </div>
+            ))}
+        </div>
+
+    )
+}
+
+export default Bloglist
