ac6f1e4f92537c4fde5a420fb30ae262b0aa5a4d
ayomide <sulaimanayomide005@gmail.com>
2025-04-27 23:39:06 +0100

    fetch json endpoint

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,11 +3,7 @@ import BlogList from "./BlogList";
 
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        {title: "My new Website", body: "Lorem Ipsum....", author: "mario", successor: "Child", id: 1},
-        {title: "Welcome Party!", body: "Lorem Ipsum....", author: "yoshi", successor: "Niece",  id: 2},
-        {title: "Web dev top tips", body: "Lorem Ipsum....", author: "luigi", successor: "Child",  id: 3}
-    ])
+    const [blogs, setBlogs] = useState(null)
 
     const handleDelete = (id) =>{
         const newBlog = blogs.filter((blog) => blog.id !== id)
@@ -17,13 +13,19 @@ const Home = () => {
     const [name, setName] = useState('Hamid')
 
 useEffect(() => {
-    console.log("UseEffect ran");
-}, [blogs]) //empty dependency array [only run once when the component mounts]
+    fetch('http://localhost:8000/blogs')
+        .then((res) =>{
+            return res.json()
+        })
+        .then((data) =>{
+            setBlogs(data)
+        })
+}, []) //empty dependency array [only run once when the component mounts]
 
 
     return (
         <div className="home">
-            <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/>
+            {blogs && <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/>}
             {/* <BlogList blogs={blogs.filter((blog) => blog.successor === "Child")} title="Another Blogs" /> */}
             <button onClick={() => setName('Sulaiman')}>Change Name</button>
             <p>{name}</p>
