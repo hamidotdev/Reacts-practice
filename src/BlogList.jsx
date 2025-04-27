d361cd7e24b5a64f742353fd607061dfde05c1f8
ayomide <sulaimanayomide005@gmail.com>
2025-04-27 16:08:46 +0100

    handle event with prop

--- src/BlogList.jsx
+++ src/BlogList.jsx
@@ -1,15 +1,19 @@
-const Bloglist = (props) => {
+const BlogList = (props) => {
     const { blogs } = props; // Destructure the blogs prop
     // const blogs = props.blogs; // Alternatively, you can use this line to access the blogs prop
     // const blogs = [ // Sample data for testing
-    const title = props.title; // Destructure the title prop
+    // const title = props.title; // Destructure the title prop
+    const { handleDelete } = props
+    console.log(props);
     return (
-        <div className="Bloglist">
-            <h1>{title}</h1>
+        <div className="blog-list">
+            {/* <h1>{title}</h1> */}
             {blogs.map((blog) => (
                 <div className="blog-preview" key={blog.id}>
                     <h1>{blog.title}</h1>
                     <p>Written by {blog.author}</p>
+                    <p>Successor: {blog.successor}</p>
+                    <button onClick={() => handleDelete(blog.id)}>Delete Blog</button>
                 </div>
             ))}
         </div>
@@ -17,4 +21,4 @@ const Bloglist = (props) => {
     )
 }
 
-export default Bloglist
+export default BlogList
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,16 +1,24 @@
 import { useState } from "react";
-import Bloglist from "./Bloglist";
+import BlogList from "./BlogList";
+
 
 const Home = () => {
     const [blogs, setBlogs] = useState([
-        {title: "My new Website", body: "Lorem Ipsum....", author: "mario", id: 1},
-        {title: "Welcome Party!", body: "Lorem Ipsum....", author: "yoshi", id: 2},
-        {title: "Web dev top tips", body: "Lorem Ipsum....", author: "luigi", id: 3}
+        {title: "My new Website", body: "Lorem Ipsum....", author: "mario", successor: "Child", id: 1},
+        {title: "Welcome Party!", body: "Lorem Ipsum....", author: "yoshi", successor: "Niece",  id: 2},
+        {title: "Web dev top tips", body: "Lorem Ipsum....", author: "luigi", successor: "Child",  id: 3}
     ])
+
+    const handleDelete = (id) =>{
+        const newBlog = blogs.filter((blog) => blog.id !== id)
+        setBlogs(newBlog)
+    }
+
 
     return (
         <div className="home">
-            <Bloglist  blogs= {blogs} title= "HomePage"/>
+            <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/>
+            {/* <BlogList blogs={blogs.filter((blog) => blog.successor === "Child")} title="Another Blogs" /> */}
         </div>
     )
 }
