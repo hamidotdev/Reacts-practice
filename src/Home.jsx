e93dd4c19ac5063ae1a1fbcba986969a905606c4
ayomide <sulaimanayomide005@gmail.com>
2025-04-28 07:29:11 +0100

    error message loading

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,6 +4,8 @@ import BlogList from "./BlogList";
 
 const Home = () => {
     const [blogs, setBlogs] = useState(null)
+    const [isLoading, setIsLoading] = useState(true)
+    const [errors, setErrors] = useState(null)
 
     const handleDelete = (id) =>{
         const newBlog = blogs.filter((blog) => blog.id !== id)
@@ -13,18 +15,30 @@ const Home = () => {
     const [name, setName] = useState('Hamid')
 
 useEffect(() => {
-    fetch('http://localhost:8000/blogs')
-        .then((res) =>{
-            return res.json()
-        })
-        .then((data) =>{
-            setBlogs(data)
-        })
+    setTimeout(() => {
+        fetch('http://localhost:8000/blogs')
+            .then((res) =>{
+                if (!res.ok) {
+                    throw Error("Could not fetch data from that resource")
+                }
+                return res.json()
+            })
+            .then((data) => {
+                setBlogs(data)
+                setIsLoading(false)
+            })
+            .catch((error) => {
+                setErrors(error.message)
+                setIsLoading(false)
+            })
+    }, 1000);
 }, []) //empty dependency array [only run once when the component mounts]
 
 
     return (
         <div className="home">
+            {errors && <div className="error">{errors}</div>}
+            {isLoading && <div className="loading"> ....Loading... </div>}
             {blogs && <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/>}
             {/* <BlogList blogs={blogs.filter((blog) => blog.successor === "Child")} title="Another Blogs" /> */}
             <button onClick={() => setName('Sulaiman')}>Change Name</button>
